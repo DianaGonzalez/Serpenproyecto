56f337f24a1326ea42db2298131237c097b32234
Edgar Meneses <Edgar Meneses@AntoniusHack-PC>
2015-03-28 12:10:48 -0500

    Paquete de interface grafica

---
+++ src/com/serpen/logueo/presentation/ChangePasword.java
@@ -0,0 +1,14 @@
+package com.serpen.logueo.presentation;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+public class ChangePasword extends UI{
+
+	@Override
+	protected void init(VaadinRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
